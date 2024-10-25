5327ee3c7d7d0adbba0cd30baf547c96c7d77fa9
Nat1823 <122820838+Nat1823@users.noreply.github.com>
2024-10-25 12:31:00 -0500

    ms_pay con excepciones

---
+++ ms_pay/ms_pay/src/main/java/com/betek/ms_pay/models/ReservationStatus.java
@@ -0,0 +1,12 @@
+package com.betek.ms_pay.models;
+
+public enum ReservationStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    PAID,
+    REFUNDED,
+    ON_HOLD,
+    COMPLETED,
+    NO_SHOW
+}
